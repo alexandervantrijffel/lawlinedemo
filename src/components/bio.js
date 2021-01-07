0c8c029b856dae54252413da4c6601832cd83b82
Alexander van Trijffel <Alexander>
2021-01-07 17:52:59 +0100

    update logo

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,7 @@ const Bio = () => {
     query BioQuery {
       avatar: file(absolutePath: { regex: "/visionisers-logo.png/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50, quality: 95) {
+          fixed(width: 40, height: 48, quality: 95) {
             ...GatsbyImageSharpFixed
           }
         }
